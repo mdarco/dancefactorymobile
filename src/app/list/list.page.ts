9fb7fb4679ecd6ddb9c5ad88b4cfb9824c6d6511
Darko Milutinović <dmilutinovic@gmail.com>
2019-01-13 19:08:20 +0100

    members page changes

--- src/app/list/list.page.ts
+++ src/app/list/list.page.ts
@@ -6,34 +6,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['list.page.scss']
 })
 export class ListPage implements OnInit {
-  private selectedItem: any;
-  private icons = [
-    'flask',
-    'wifi',
-    'beer',
-    'football',
-    'basketball',
-    'paper-plane',
-    'american-football',
-    'boat',
-    'bluetooth',
-    'build'
-  ];
-  public items: Array<{ title: string; note: string; icon: string }> = [];
+  public members: Array<any> = [];
+  
   constructor() {
-    for (let i = 1; i < 11; i++) {
-      this.items.push({
-        title: 'Item ' + i,
-        note: 'This is item #' + i,
-        icon: this.icons[Math.floor(Math.random() * this.icons.length)]
-      });
-    }
+    this.members = [
+      { FullName: 'Pera Detlic', IsActive: true },
+      { FullName: 'Mornar Popaj', IsActive: false },
+      { FullName: 'Lale Gator', IsActive: true }
+    ];
   }
 
-  ngOnInit() {
-  }
-  // add back when alpha.4 is out
-  // navigate(item) {
-  //   this.router.navigate(['/list', JSON.stringify(item)]);
-  // }
+  ngOnInit() { }
 }
