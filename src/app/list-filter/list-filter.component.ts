4f8d98b6b4beea434687e13fd23b8749a6461ee2
Darko Milutinović <dmilutinovic@gmail.com>
2019-01-15 23:05:14 +0100

    list filter done

--- src/app/list-filter/list-filter.component.ts
+++ src/app/list-filter/list-filter.component.ts
@@ -1,17 +1,59 @@
-import { Component, OnInit } from '@angular/core';
-import { ModalController } from '@ionic/angular';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { LoadingController, ToastController, ModalController } from '@ionic/angular';
+
+import { DanceGroupsService } from '../services/dance-groups/dance-groups.service';
 
 @Component({
   selector: 'app-list-filter',
   templateUrl: './list-filter.component.html',
   styleUrls: ['./list-filter.component.scss']
 })
-export class ListFilterComponent implements OnInit {
+export class ListFilterComponent implements OnInit, OnDestroy {
+  danceGroups: any = [];
   modalData = {};
 
-  constructor(private modalController: ModalController) { }
+  private danceGroups$: any;
 
-  ngOnInit() { }
+  constructor(
+    private loadingController: LoadingController,
+    private toastController: ToastController,
+    private modalController: ModalController,
+    private danceGroupsService: DanceGroupsService
+  ) { }
+
+  ngOnInit() {
+    this.getDanceGroups();
+  }
+
+  ngOnDestroy() {
+    this.danceGroups$.unsubscribe();
+  }
+
+  async getDanceGroups() {
+    const loading = await this.loadingController.create({
+      spinner: 'circles',
+      message: 'Učitavanje grupa u toku..'
+    });
+
+    await loading.present();
+
+    this.danceGroups$ = this.danceGroupsService.getLookup().subscribe(
+      response => {
+        console.log('DANCE GROUPS RESPONSE', response);
+        if (response && response['length'] > 0) {
+          this.danceGroups = response;
+        }
+      },
+      error => {
+        console.log('DANCE GROUPS LOOKUP ERROR', error);
+        this.danceGroups = [];
+        this.showToast('Došlo je do greške prilikom preuzimanja spiska plesnih grupa.', 'danger');
+      },
+      () => {
+        loading.dismiss();
+      }
+    );
+  }
 
   removeStatusSelection() {
     this.modalData['Status'] = undefined;
@@ -19,6 +61,16 @@ export class ListFilterComponent implements OnInit {
 
   removeDanceGroupSelection() {
     this.modalData['DanceGroupID'] = undefined;
+  }
+
+  async showToast(message, color) {
+    const toast = await this.toastController.create({
+      message: message,
+      duration: 3000,
+      position: 'top',
+      color
+    });
+    await toast.present();
   }
 
   applyFilter() {
